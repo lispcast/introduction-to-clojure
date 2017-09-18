7ac85c6a2332013d67af72d1c27059d7942781d1
Eric Normand <ericwnormand@gmail.com>
2017-09-18 10:12:42 -0500

    Other add- functions

--- src/introduction_to_clojure/core.clj
+++ src/introduction_to_clojure/core.clj
@@ -5,3 +5,25 @@
   (grab :egg)
   (squeeze)
   (add-to-bowl))
+
+(defn add-flour []
+  (grab :cup)
+  (scoop :flour)
+  (add-to-bowl)
+  (release))
+
+(defn add-milk []
+  (grab :cup)
+  (scoop :milk)
+  (add-to-bowl)
+  (release))
+
+(defn add-sugar []
+  (grab :cup)
+  (scoop :sugar)
+  (add-to-bowl)
+  (release))
+
+(defn add-butter []
+  (grab :butter)
+  (add-to-bowl))
