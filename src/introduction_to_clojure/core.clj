e00332e9086659c2e58d153831d5a4663a9bceeb
Eric Normand <ericwnormand@gmail.com>
2017-09-27 10:11:35 -0500

    Remove add- and add-s functions

--- src/introduction_to_clojure/core.clj
+++ src/introduction_to_clojure/core.clj
@@ -5,33 +5,6 @@
   (apply println args)
   :error)
 
-(defn add-egg []
-  (grab :egg)
-  (squeeze)
-  (add-to-bowl))
-
-(defn add-flour []
-  (grab :cup)
-  (scoop :flour)
-  (add-to-bowl)
-  (release))
-
-(defn add-milk []
-  (grab :cup)
-  (scoop :milk)
-  (add-to-bowl)
-  (release))
-
-(defn add-sugar []
-  (grab :cup)
-  (scoop :sugar)
-  (add-to-bowl)
-  (release))
-
-(defn add-butter []
-  (grab :butter)
-  (add-to-bowl))
-
 (def scooped-ingredients #{:flour :sugar :milk})
 
 (defn scooped? [ingredient]
@@ -46,31 +19,6 @@
 
 (defn simple? [ingredient]
   (contains? simple-ingredients ingredient))
-
-(defn add-eggs [n]
-  (dotimes [e n]
-    (add-egg))
-  :ok)
-
-(defn add-flour-cups [n]
-  (dotimes [e n]
-    (add-flour))
-  :ok)
-
-(defn add-milk-cups [n]
-  (dotimes [e n]
-    (add-milk))
-  :ok)
-
-(defn add-sugar-cups [n]
-  (dotimes [e n]
-    (add-sugar))
-  :ok)
-
-(defn add-butters [n]
-  (dotimes [e n]
-    (add-butter))
-  :ok)
 
 (defn add-squeezed
   ([ingredient amount]
