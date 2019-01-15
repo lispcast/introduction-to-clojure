ac3dee473980f2403bd0418927062cbac5df5b56
Eric Normand <ericwnormand@gmail.com>
2019-01-15 13:05:07 -0600

    Update Clojure and Nightlight versions

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "https://purelyfunctional.tv/"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :plugins [[nightlight/lein-nightlight "2.3.2"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [lispcast/bakery "1.0.0"]]
   :repl-options {:init (do
                          (use 'bakery.core)
                          (use 'introduction-to-clojure.core))
-                 :skip-default-init true})
+                 :skip-default-init true}
+  
+
+  :main introduction-to-clojure.core)
